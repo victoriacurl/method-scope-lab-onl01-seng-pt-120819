3d930755d22a5feb0a0b16ef22f0c0c9c481fc1f
Victoria Curl <victoriacurl9@gmail.com>
2019-12-17 20:18:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,6 @@
-def catch_phrase
-  puts phrase
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
